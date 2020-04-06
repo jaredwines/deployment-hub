e9f311242aad13e35d41a0ad45c19daa8fe69882
Jared Wines <jwines760@gmail.com>
2020-04-05 22:17:03 -0700

    .

--- scripts/jareds_macbook_air/remote_deploy.sh
+++ scripts/jareds_macbook_air/remote_deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 if [ ! -z "$1" ]
 then
-	remote_deploy_script="$1"
-	if [ $remote_deploy_script == "jaredwines.com" ]
+	remote_deploy="$1"
+
+	if [ $remote_deploy == "jaredwines.com" ]
 	then
-		ssh jaredwines.com /home/jaredw/.scripts/remote_deploy_jaredwines.com.sh "$2"
+		echo "Connection to $remote_deploy."
+		ssh -q $host exit
+		result="$?"
+		if [ $result == "0" ]
+		then
+			echo "Successfully connected to $remote_deploy."
+			ssh jaredwines.com /home/jaredw/.scripts/remote_deploy_jaredwines.com.sh "$2"
+		else
+			echo "Failed to connect to $remote_deploy (Error code: $result)."
 	fi
 
 else
-	echo "First arg can NOT be empty, try [remote_deploy.sh $remote_deploy_script]."
+	echo "First arg can NOT be empty, try [remote_deploy.sh $remote_deploy]."
 fi
