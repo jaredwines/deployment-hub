b2d0ddbb883f71720ddc02ca213c5f4141d9453a
Jared Wines <jwines760@gmail.com>
2020-04-05 22:39:40 -0700

    .

--- scripts/jareds_macbook_air/remote_deploy.sh
+++ scripts/jareds_macbook_air/remote_deploy.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+excute_remote_script(){
+host="$1"
+path_to_script="$2"
+arg="$3"
+
+echo "Connection to $remote_deploy."
+ssh -q $host exit
+result="$?"
+if [ $result == "0" ]
+then
+	echo "Successfully connected to $remote_deploy."
+	ssh $host $path_to_script $arg
+else
+	echo "Failed to connect to $remote_deploy (Error code: $result)."
+fi
+}
+
 if [ ! -z "$1" ]
 then
 	remote_deploy="$1"
-
+	arg="$2"
 	if [ $remote_deploy == "jaredwines.com" ]
 	then
-		echo "Connection to $remote_deploy."
-		ssh -q $host exit
-		result="$?"
-		if [ $result == "0" ]
-		then
-			echo "Successfully connected to $remote_deploy."
-			ssh jaredwines.com /home/jaredw/.scripts/remote_deploy_jaredwines.com.sh "$2"
-		else
-			echo "Failed to connect to $remote_deploy (Error code: $result)."
-		fi
+		host=jaredwines.com
+		path_to_script=/home/jaredw/.scripts/remote_deploy_jaredwines.com.sh
+		excute_remote_script() $host $path_to_script $arg
 	fi
 
 else
