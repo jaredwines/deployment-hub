904d415b003e05cf3048ce8ebe986dfb8d5408dd
Jared Wines <jwines760@gmail.com>
2020-04-05 19:57:07 -0700

    Add dir.

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -1,40 +1,64 @@
 #!/bin/bash
 shopt -s extglob
 
-git_repo=git@github.com:jaredwines/deployment.git
-scripts_repo_dir=scripts
-scripts_dir_name=.scripts
+update_scripts(){
+	git_repo=git@github.com:jaredwines/deployment.git
+	scripts_dir=.scripts
+	scripts_repo_dir="$1"
 
-tmp_deployment_dir=$HOME/.tmp_process_update_scripts
-scripts_dir=$HOME/$scripts_dir_name
+	tmp_deployment_path=$HOME/.tmp_process_update_scripts
+	scripts_path=$HOME/$scripts_dir
+	scripts_repo_path=$tmp_deployment_path/$scripts_repo_dir/scripts
 
-echo "Downloading script(s)."
-if [ -d "$tmp_deployment_dir" ]; then
-	rm -rf $tmp_deployment_dir
-fi
-mkdir $tmp_deployment_dir
+	echo "Downloading script(s)."
+	if [ -d "$tmp_deployment_path" ]; then
+		rm -rf $tmp_deployment_path
+	fi
+	mkdir $tmp_deployment_path
 
-if git clone $git_repo $tmp_deployment_dir
+	if git clone $git_repo $tmp_deployment_path
+	then
+		echo "Download complete."
+	else
+		echo "Download fail."
+		exit 1
+	fi
+
+	echo "Installing script(s)."
+	if [ -d "$scripts_path" ] || [ "$(ls -A $scripts_path)" ]
+	then
+		rm -rf $scripts_path/*
+	elif [ ! -d "$scripts_path" ]
+	then
+		mkdir $scripts_path
+	fi
+
+	if mv $scripts_repo_path/* $scripts_path && chmod +x $scripts_path/*.sh && rm -rf $tmp_deployment_path
+	then
+		echo "Successfully installed script(s)."
+	else
+		echo "Failed to install script(s)."
+		exit 1
+	fi
+}
+
+if [ $# -eq 0 ]
 then
-	echo "Download complete."
+	update_scripts jareds_macbook_air
+elif [ $# -eq 1 ]
+then
+	host="$1"
+
+	echo "Connection to $host."
+	ssh -q $host exit
+	result="$?"
+	if [ $result == "0" ]
+	then
+		echo "Successfully connected to $host."
+		ssh $host "$(typeset -f update_scripts); update_scripts $1"
+	else
+		echo "Failed to connect to $host (Error code: $result)."
+	fi
 else
-	echo "Download fail."
-	exit 1
-fi
-
-echo "Installing script(s)."
-if [ -d "$scripts_dir" ] && [ "$(ls -A $scripts_dir)" ]
-then
-	rm -rf $scripts_dir/*
-elif [ ! -d "$scripts_dir" ]
-then
-	mkdir $scripts_dir
-fi
-
-if mv $tmp_deployment_dir/$scripts_repo_dir/* $scripts_dir && chmod +x $scripts_dir/*.sh && rm -rf $tmp_deployment_dir
-then
-	echo "Successfully installed script(s)."
-else
-	echo "Failed to install script(s)."
-	exit 1
+	echo "Too many args, try [update_scripts.sh] or [update_scripts.sh $host]."
 fi
