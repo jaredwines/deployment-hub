13fedb2872e019e913b147d20a56b20454be6244
Jared Wines <jwines760@gmail.com>
2020-04-05 21:02:29 -0700

    .

---
+++ scripts/jareds_macbook_air/remote_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+	remote_deploy_script="$1"
+	if [ $remote_deploy_script == "jaredwines.com"]
+	then
+		ssh jaredwines.com /home/jaredw/.scripts/remote_deploy_jaredwines.com.sh "$2"
+	fi
+
+else
+	echo "First arg can NOT be empty, try [remote_deploy.sh $remote_deploy_script]."
+fi
