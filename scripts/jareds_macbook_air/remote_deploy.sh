ada0063295750489019046e9a6f394ac23e0b835
Jared Wines <jwines760@gmail.com>
2020-04-05 22:48:36 -0700

    .

--- scripts/jareds_macbook_air/remote_deploy.sh
+++ scripts/jareds_macbook_air/remote_deploy.sh
@@ -24,6 +24,7 @@ then
 	then
 		host=jaredwines.com
 		path_to_script=/home/jaredw/.scripts/remote_deploy_jaredwines.com.sh
+		echo asdf
 		excute_remote_script "$host" "$path_to_script" "$arg"
 	fi
 
