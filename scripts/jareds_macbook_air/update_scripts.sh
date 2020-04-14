5d71e1488e599d8f650e9da0690a62d9683e88a8
Jared Wines <jwines760@gmail.com>
2020-04-14 11:54:03 -0700

    Fix error.

--- scripts/jareds_macbook_air/update_scripts.sh
+++ scripts/jareds_macbook_air/update_scripts.sh
@@ -5,12 +5,14 @@ update_scripts(){
 	git_repo=git@github.com:jaredwines/deployment.git
 	scripts_dir=.scripts
 	scripts_repo_dir="$1"
+	host="$1"
 
 	tmp_deployment_path=$HOME/.tmp_process_update_scripts
 	scripts_path=$HOME/$scripts_dir
 	scripts_repo_path=$tmp_deployment_path/scripts/$scripts_repo_dir
 
-	echo "Downloading script(s) to $scripts_repo_dir."
+	echo "Starting update for $host's scripts."
+	echo "Downloading script(s) to $host."
 	if [ -d "$tmp_deployment_path" ]; then
 		rm -rf $tmp_deployment_path
 	fi
@@ -24,7 +26,7 @@ update_scripts(){
 		exit 1
 	fi
 
-	echo "Installing script(s) to $scripts_repo_dir."
+	echo "Installing script(s) to $host."
 	if [ -d "$scripts_path" ] || [ "$(ls -A $scripts_path)" ]
 	then
 		rm -rf $scripts_path/*
@@ -35,11 +37,13 @@ update_scripts(){
 
 	if ( mv $scripts_repo_path/* $scripts_path && chmod +x $scripts_path/*.sh && rm -rf $tmp_deployment_path )
 	then
-		echo "Successfully installed script(s) to $scripts_repo_dir."
+		echo "Successfully installed script(s) to $host."
 	else
-		echo "Failed to install script(s) to $scripts_repo_dir."
+		echo "Failed to install script(s) to $host."
 		exit 1
 	fi
+
+	echo "Finshed update for $host's scripts."
 }
 
 if [ $# -eq 0 ]
@@ -49,6 +53,11 @@ else
 	host="$1"
 	run_update_scripts_jareds_macbook_air="$2"
 
+	if [ "$run_update_scripts_jareds_macbook_air" == "-l" ]
+	then
+		update_scripts jareds_macbook_air
+	fi
+	
 	echo "Connection to $host."
 	ssh -q $host exit
 	result="$?"
@@ -56,10 +65,6 @@ else
 	then
 		echo "Successfully connected to $host."
 		ssh $host "$(typeset -f update_scripts); update_scripts $1"
-		if [ "$run_update_scripts_jareds_macbook_air" == "-l" ]
-		then
-			update_scripts jareds_macbook_air
-		fi
 	else
 		echo "Failed to connect to $host (Error code: $result)."
 		exit 1
