9f4180e76c99616f6437a1af65cdcfc16046c931
Jared Wines <jwines760@gmail.com>
2020-12-11 20:52:22 -0800

    Refactor core logic.

--- smart-hub/deploy-hub.py
+++ smart-hub/deploy-hub.py
@@ -9,7 +9,8 @@ def excute_remote_script(host, path_to_script, arg) :
 
     if result == "0" :
         print "Successfully connected to " + host + "."
-        os.system("ssh " + host + path_to_script + arg)
+    	os.system("ssh " + host + "'bash -s' < " + path_to_script + arg)
+		
     else :
         print "Failed to connect to $host (Error code: " + result + ")."
 
@@ -23,7 +24,7 @@ parser.read('/Users/jared/Repository/deployment/scripts/config.ini')
 
 if len(sys.argv) == 1 :
 	deployment=sys.argv[1]
-	arg=sys.argv[2]
+	arg=' '.join(sys.argv[2:])
 
 	if parser.has_section(deployment) :
 		host=parser.get(deployment, 'host')
--- smart-hub/deployment-scripts/deploy_jaredwines.com.sh
+++ smart-hub/deployment-scripts/deploy_jaredwines.com.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 shopt -s extglob
 
-
 git_repo=git@github.com:jaredwines/jaredwines.com.git
+branch=master
 website_dir_name=jaredwines.com
 website_url=www.jaredwines.com
 
 tmp_deploy_dir=$HOME/.tmp_process_deploy_$website_url
 website_dir=$HOME/$website_dir_name
-maintenance_mode="$1"
+
+maintenance_mode=false
+
+while [ ! -z "$1" ]; do
+  case "$1" in
+     --maintenance|-m)
+         shift
+         maintenance_mode=true
+         ;;
+     --branch|-b)
+         shift
+         branch=${@: -1} 
+         ;;
+  esac
+shift
+done
 
 # Create temporary directory for repository.
 echo "Downloading $website_url."
@@ -18,7 +33,7 @@ fi
 mkdir $tmp_deploy_dir
 
 # Clone website repository.
-if git clone $git_repo $tmp_deploy_dir
+if git clone -b $branch $git_repo $tmp_deploy_dir
 then
 	echo "Download completed."
 else
@@ -40,7 +55,7 @@ fi
 if ( mv $tmp_deploy_dir/*!(.git*) $website_dir && rm -rf $tmp_deploy_dir )
 then
 	# Enable maintenance mode if arg="-m" else disable maintenance mode.
-	if [ ! -z "$maintenance_mode" ] && [ $maintenance_mode == "-m" ]
+	if [ $maintenance_mode == true ]
 	then
 		sed -i 's/RewriteEngine Off/RewriteEngine On/g' $HOME/.htaccess
 		echo "Maintenance mode is enable."
