bcac3d9e34188415711a434320d7e202665d5682
Jared Wines <jwines760@gmail.com>
2020-12-13 12:01:19 -0800

    Overload Jaredwines constructor.

--- smart-hub2/jaredwines_com_deployment.py
+++ smart-hub2/jaredwines_com_deployment.py
@@ -1,7 +1,19 @@
 from deployment import Deployment
 
 class JaredWinesComDeployment(Deployment): 
+
+    def __init__(self, host, gitRepo, branch, maintenance_flag = False):
+        self._maintenance_flag = maintenance_flag
+        super().__init__(host, gitRepo, branch)
+
+    @property
+    def maintenance_flag(self):
+        return self._maintenance_flag
+
+    @maintenance_flag.setter
+    def maintenance_flag(self,maintenance_flag):
+        self._maintenance_flag=maintenance_flag
    
     def deploy(self):    
-       return self.branch + self.host + self.gitRepo 
+       return self.branch + self.host + self.gitRepo + self.maintenance_flag
   
