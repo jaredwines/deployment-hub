fccd0221729bb1254728a37c33d922a367c96dcd
Jared Wines <jwines760@gmail.com>
2021-01-09 16:37:50 -0800

    Add logic.

--- src/deployment/apps/jared_wines_com_deployment.py
+++ src/deployment/apps/jared_wines_com_deployment.py
@@ -1,7 +1,7 @@
-from ..model.deployment import Deployment
-from ..deployment.deployment_command import DeploymentCommand
-from ..deployment.docker_command import DockerCommand
-from ..model.ssh_deployment_client import SshDeploymentClient
+from ...model.deployment import Deployment
+from ...deployment.deployment_command import DeploymentCommand
+from ...deployment.docker_command import DockerCommand
+from ...model.ssh_deployment_client import SshDeploymentClient
 import os
 
 class JaredWinesComDeployment(DeploymentCommand): 
