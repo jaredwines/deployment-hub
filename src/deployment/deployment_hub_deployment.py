7bcf92fff0e05289d62b0f0fd2ab0830bbcc6c4a
jaredwines <jared.wines@pricefx.com>
2023-07-25 14:36:49 -0700

    Add back up home assistant.

--- src/deployment/deployment_hub_deployment.py
+++ src/deployment/deployment_hub_deployment.py
@@ -13,7 +13,7 @@ class DeploymentHubDeployment:
 
     def update(self):
         self.__ssh_deployment_client.exec_command(
-            "sed -i '/DEPLOYMENT_HUB_VERSION/c\DEPLOYMENT_HUB_VERSION=" + self.__deployment.branch + "' " +
+            "sed -i '/DEPLOYMENT_HUB_BRANCH/c\DEPLOYMENT_HUB_BRANCH=" + self.__deployment.branch + "' " +
             self.__deployment.project_dir + "/.env")
         self.__ssh_deployment_client.exec_command(
             "docker-compose --env-file " + self.__deployment.project_dir + "/.env --file " + self.__deployment.project_dir + "/docker-compose.yml up --force-recreate --build -d")
--- src/deployment/homebridge_deployment.py
+++ src/deployment/homebridge_deployment.py
@@ -1,48 +1,48 @@
-# from src.deployment.util.deployment_file_util import DeploymentFileUtil
-# from src.deployment.util.ssh_deployment_client import SshDeploymentClient
-# from src.model.deployment import Deployment
-#
-#
-# class HomebridgeDeployment:
-#
-#     def __init__(self, branch="main"):
-#         self.__deployment = Deployment("git@github.com:jaredwines/homebridge-config.git", branch,
-#                                        "/home/jared/Projects/homebridge-config")
-#         self.__ssh_deployment_client = SshDeploymentClient("nuc")
-#         self._deployment_util = DeploymentFileUtil(self.__deployment, self.__ssh_deployment_client)
-#
-#     def start_docker(self):
-#         self.__ssh_deployment_client.exec_command(
-#             "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml up -d")
-#
-#     def restart_docker(self):
-#         self.__ssh_deployment_client.exec_command(
-#             "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml restart")
-#
-#     def stop_docker(self):
-#         self.__ssh_deployment_client.exec_command(
-#             "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml stop")
-#
-#     def update_docker(self):
-#         self.__ssh_deployment_client.exec_command(
-#             "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml pull")
-#         self.__ssh_deployment_client.exec_command(
-#             "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml up -d --build homeassistant")
-#
-#     def backup_homebridge(self):
-#         command_list = []
-#
-#         command_list.append("git -C " + self.__deployment.project_dir + " pull")
-#         command_list.append("git -C " + self.__deployment.project_dir + " add --all")
-#         command_list.append("git -C " + self.__deployment.project_dir + " commit -m \"Backup.\"")
-#         command_list.append("git -C " + self.__deployment.project_dir + " push")
-#
-#         self.__ssh_deployment_client.exec_command(command_list)
-#
-#     def deploy(self):
-#         self._deployment_util.create_tmp_dir()
-#         self._deployment_util.clone_git_repo()
-#         self._deployment_util.move_deployment_contents()
-#         self._deployment_util.remove_tmp_dir()
-#
-#         self.restart_docker()
+from src.deployment.util.deployment_file_util import DeploymentFileUtil
+from src.deployment.util.ssh_deployment_client import SshDeploymentClient
+from src.model.deployment import Deployment
+
+
+class HomebridgeDeployment:
+
+    def __init__(self, branch="main"):
+        self.__deployment = Deployment("git@github.com:jaredwines/homebridge-config.git", branch,
+                                       "/home/jared/Projects/homebridge-config")
+        self.__ssh_deployment_client = SshDeploymentClient("nuc")
+        # self._deployment_util = DeploymentFileUtil(self.__deployment, self.__ssh_deployment_client)
+
+    # def start_docker(self):
+    #     self.__ssh_deployment_client.exec_command(
+    #         "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml up -d")
+    #
+    # def restart_docker(self):
+    #     self.__ssh_deployment_client.exec_command(
+    #         "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml restart")
+    #
+    # def stop_docker(self):
+    #     self.__ssh_deployment_client.exec_command(
+    #         "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml stop")
+    #
+    # def update_docker(self):
+    #     self.__ssh_deployment_client.exec_command(
+    #         "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml pull")
+    #     self.__ssh_deployment_client.exec_command(
+    #         "docker-compose -f " + self.__deployment.project_dir + "/docker-compose.yml up -d --build homeassistant")
+    #
+    # def backup_homebridge(self):
+    #     command_list = []
+    #
+    #     command_list.append("git -C " + self.__deployment.project_dir + " pull")
+    #     command_list.append("git -C " + self.__deployment.project_dir + " add --all")
+    #     command_list.append("git -C " + self.__deployment.project_dir + " commit -m \"Backup.\"")
+    #     command_list.append("git -C " + self.__deployment.project_dir + " push")
+    #
+    #     self.__ssh_deployment_client.exec_command(command_list)
+    #
+    # def deploy(self):
+    #     self._deployment_util.create_tmp_dir()
+    #     self._deployment_util.clone_git_repo()
+    #     self._deployment_util.move_deployment_contents()
+    #     self._deployment_util.remove_tmp_dir()
+    #
+    #     self.restart_docker()
