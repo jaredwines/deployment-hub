4a4f92d76d56ad5c5d774e41fc1d9dfd3fdde311
jaredwines <jared.wines@pricefx.com>
2023-07-25 17:57:14 -0700

    Add back up home assistant.

--- src/deployment/home_assistant_deployment.py
+++ src/deployment/home_assistant_deployment.py
@@ -7,7 +7,7 @@ PROJECT_DIR = "/home/jared/Projects/homeassistant-config"
 SSH_HOSTNAME = "nuc"
 
 
-class HomeAssistantDeployment(DeploymentDockerUtil, ):
+class HomeAssistantDeployment(DeploymentDockerUtil):
 
     def __init__(self, branch="main"):
         self._deployment = Deployment(GIT_URL, branch, PROJECT_DIR, SshDeploymentClient(SSH_HOSTNAME))
