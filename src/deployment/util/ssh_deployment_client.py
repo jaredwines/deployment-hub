2f7f3191807b13c0461d17f4df3749f5f024011b
jaredwines <jared.wines@pricefx.com>
2023-07-28 11:32:49 -0700

    Add back up home assistant.

--- src/deployment/util/ssh_deployment_client.py
+++ src/deployment/util/ssh_deployment_client.py
@@ -49,9 +49,10 @@ class SshDeploymentClient:
             for i in range(length):
                 if i is (length - 1):
                     output_list_str += output_list[i].rstrip()
-                    output_list[i] = output_list[i].rstrip()
                 else:
                     output_list_str += output_list[i]
+
+                output_list[i] = output_list[i].rstrip()
 
             current_app.logger.info(output_list_str)
 
