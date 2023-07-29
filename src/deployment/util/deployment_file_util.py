0c29c4566bcdd68b912e8cf7b9ca60de14298370
jaredwines <jared.wines@pricefx.com>
2023-07-29 13:51:28 -0700

    Add back up home assistant.

--- src/deployment/util/deployment_file_util.py
+++ src/deployment/util/deployment_file_util.py
@@ -28,6 +28,7 @@ class DeploymentFileUtil(DeploymentGitUtil):
         if target_dir is None:
             target_dir = self.__project_dir
 
+asdfa
         include_list_command_str = ""
         exclude_list_command_str = "--exclude='.git' --exclude='.gitignore' "
 
