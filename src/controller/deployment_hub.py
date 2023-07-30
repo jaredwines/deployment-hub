fc85a18677f84763944926fca408f6dfa511a9a1
jaredwines <jared.wines@pricefx.com>
2023-07-29 23:43:30 -0700

    try removing cross-origin decorator

--- src/controller/deployment_hub.py
+++ src/controller/deployment_hub.py
@@ -17,7 +17,6 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 @app.route('/project-options/', methods=['GET'])
-@cross_origin()
 def get_project_options():
     return True
 
@@ -26,9 +25,7 @@ def prepareResponse(response):
     return jsonify({"response": response})
 
 @app.route('/<project>/<action>/', methods=['POST', 'GET'])
-@cross_origin()
 @app.route('/<project>/<action>/<branch>/', methods=['POST', 'GET'])
-@cross_origin()
 def deploy(project=None, branch=None, action=None):
     if project == "alohamillworks":
         if branch is None:
