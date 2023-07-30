c6661bcd7e8746668d1770bac801a06e237366f4
Fransheska Colon <franc@alum.mit.edu>
2023-07-29 21:32:47 -0700

    added jsonify

--- src/controller/deployment_hub.py
+++ src/controller/deployment_hub.py
@@ -1,5 +1,5 @@
 import logging
-
+import json
 from flask import Flask
 
 from src.deployment.aloha_millworks_com_deployment import AlohaMillworksDeployment
@@ -18,6 +18,8 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 def get_project_options():
     return True
 
+def jsonify(response):
+    return json.dumps(response)
 
 @app.route('/<project>/<action>/', methods=['POST', 'GET'])
 @app.route('/<project>/<action>/<branch>/', methods=['POST', 'GET'])
@@ -29,14 +31,14 @@ def deploy(project=None, branch=None, action=None):
             aloha_millworks = AlohaMillworksDeployment(branch)
 
         if action == "deploy":
-            return aloha_millworks.deploy()
+            return jsonify(aloha_millworks.deploy())
 
         elif action == "backup":
-            return aloha_millworks.backup()
+            return jsonify(aloha_millworks.backup())
 
         # if action == "maintenance-mode":
         #     aloha_millworks.maintenance_flag = "True"
-        #     return aloha_millworks.deploy()
+        #     return jsonify(aloha_millworks.deploy()
 
     if project == "coastalteardrops":
         if branch is None:
@@ -45,14 +47,14 @@ def deploy(project=None, branch=None, action=None):
             coastal_teardrops = CoastalTeardropsDeployment(branch)
 
         if action == "deploy":
-            return coastal_teardrops.deploy()
+            return jsonify(coastal_teardrops.deploy())
 
         elif action == "backup":
-            return coastal_teardrops.backup()
+            return jsonify(coastal_teardrops.backup())
 
         # if action == "maintenance-mode":
         #     coastal_teardrops.maintenance_flag = "True"
-        #     return coastal_teardrops.deploy()
+        #     return jsonify(coastal_teardrops.deploy()
 
     if project == "jaredwines":
         if branch is None:
@@ -61,14 +63,14 @@ def deploy(project=None, branch=None, action=None):
             jared_wines = JaredWinesComDeployment(branch)
 
         if action == "deploy":
-            return jared_wines.deploy()
+            return jsonify(jared_wines.deploy())
 
         elif action == "backup":
-            return jared_wines.backup()
+            return jsonify(jared_wines.backup())
 
         # if action == "maintenance-mode":
         #     jared_wines_com.maintenance_flag = "True"
-        #     return jared_wines_com.deploy()
+        #     return jsonify(jared_wines_com.deploy()
 
     if project == "home-assistant":
         if branch is None:
@@ -77,22 +79,22 @@ def deploy(project=None, branch=None, action=None):
             home_assistant = HomeAssistantDeployment(branch)
 
         if action == "deploy":
-            return home_assistant.deploy()
+            return jsonify(home_assistant.deploy())
 
         elif action == "start":
-            return home_assistant.start_docker()
+            return jsonify(home_assistant.start_docker())
 
         elif action == "stop":
-            return home_assistant.stop_docker()
+            return jsonify(home_assistant.stop_docker())
 
         elif action == "restart":
-            return home_assistant.restart_docker()
+            return jsonify(home_assistant.restart_docker())
 
         elif action == "update":
-            return home_assistant.update_docker()
+            return jsonify(home_assistant.update_docker())
 
         elif action == "backup":
-            return home_assistant.backup()
+            return jsonify(home_assistant.backup())
 
     if project == "homebridge":
         if branch is None:
@@ -101,22 +103,22 @@ def deploy(project=None, branch=None, action=None):
             homebridge = HomebridgeDeployment(branch)
 
         if action == "deploy":
-            return homebridge.deploy()
+            return jsonify(homebridge.deploy())
 
         elif action == "start":
-            return homebridge.start_docker()
+            return jsonify(homebridge.start_docker())
 
         elif action == "stop":
-            return homebridge.stop_docker()
+            return jsonify(homebridge.stop_docker())
 
         elif action == "restart":
-            return homebridge.restart_docker()
+            return jsonify(homebridge.restart_docker())
 
         elif action == "update":
-            return homebridge.update_docker()
+            return jsonify(homebridge.update_docker())
 
         elif action == "backup":
-            return homebridge.backup()
+            return jsonify(homebridge.backup())
 
     if project == "deployment-hub":
         if branch is None:
@@ -125,22 +127,22 @@ def deploy(project=None, branch=None, action=None):
             deployment_hub = DeploymentHubDeployment(branch)
 
         if action == "deploy":
-            return deployment_hub.deploy()
+            return jsonify(deployment_hub.deploy())
 
         elif action == "start":
-            return deployment_hub.start_docker()
+            return jsonify(deployment_hub.start_docker())
 
         elif action == "stop":
-            return deployment_hub.stop_docker()
+            return jsonify(deployment_hub.stop_docker())
 
         elif action == "restart":
-            return deployment_hub.restart_docker()
+            return jsonify(deployment_hub.restart_docker())
 
         elif action == "update":
-            return deployment_hub.update_docker()
+            return jsonify(deployment_hub.update_docker())
 
         elif action == "backup":
-            return deployment_hub.backup()
+            return jsonify(deployment_hub.backup())
 
     if project == "deployment-hub-ui":
         if branch is None:
@@ -149,22 +151,22 @@ def deploy(project=None, branch=None, action=None):
             deployment_hub_ui = DeploymentHubUIDeployment(branch)
 
         if action == "deploy":
-            return deployment_hub_ui.deploy()
+            return jsonify(deployment_hub_ui.deploy())
 
         elif action == "start":
-            return deployment_hub_ui.start_docker()
+            return jsonify(deployment_hub_ui.start_docker())
 
         elif action == "stop":
-            return deployment_hub_ui.stop_docker()
+            return jsonify(deployment_hub_ui.stop_docker())
 
         elif action == "restart":
-            return deployment_hub_ui.restart_docker()
+            return jsonify(deployment_hub_ui.restart_docker())
 
         elif action == "update":
-            return deployment_hub_ui.update_docker()
+            return jsonify(deployment_hub_ui.update_docker())
 
         elif action == "backup":
-            return deployment_hub_ui.backup()
+            return jsonify(deployment_hub_ui.backup())
 
 
 if __name__ == '__main__':
