ee35ae26e603c4b06b167d33648c73c1ededcfd6
jaredwines <jared.wines@pricefx.com>
2023-07-31 14:26:05 -0700

    Add back up home assistant.

--- src/controller/deployment_hub.py
+++ src/controller/deployment_hub.py
@@ -36,4 +36,4 @@ def deploy(project=None, action=None, branch=None):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5555, debug=True)
