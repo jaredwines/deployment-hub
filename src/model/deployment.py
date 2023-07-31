e2932e95c09fd19ca0cf970adeb777b61576e3b5
jaredwines <jared.wines@pricefx.com>
2023-07-30 21:25:27 -0700

    Add back up home assistant.

--- src/model/deployment.py
+++ src/model/deployment.py
@@ -9,7 +9,7 @@ class Deployment:
         self._project_dir = projectConfig.get('projectDir')
         self._tmp_deploy_dir = projectConfig.get('projectDir') + "/.tmp_deploy_process"
         self._branch = projectConfig.get('defaultBranch') if branch is None else branch
-        self._ssh_deployment_client = SshDeploymentClient(projectConfig.get('"hostName'))
+        self._ssh_deployment_client = SshDeploymentClient(projectConfig.get('hostName'))
         self._deployment_type = projectConfig.get('deploymentType')
         self._deploy_include_list = projectConfig.get('deployIncludeList')
         self._deploy_exclude_list = projectConfig.get('deployExcludeList')
